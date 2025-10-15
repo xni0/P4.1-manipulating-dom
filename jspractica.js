0f41444876b172b2cdff19845584d66f866bdf11
xni0 <alumno.206012@ies-azarquiel.es>
2025-10-15 17:59:52 +0200

    Initializing the repository + set up container for cards

---
+++ jspractica.js
@@ -0,0 +1,26 @@
+/**
+ * This code is just to read the json file. Don't worry about it. We will see it in detail in next sectioins
+ * Write your own code in the procesarJSON function
+ */
+
+/**
+ * Este código es solo para leeer el archivo json. No os preocupéis por él, lo veremos y lo analizaremos en próximos capítulos
+ * Escribir vuestro código en la función procesarJSON
+ */
+
+fetch("./data/heroes.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((jsondata) => {
+    console.log(jsondata);
+    renderCards(jsondata);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+function renderCards(jsondata) {
+  for (let char of jsondata.data.results) {
+  }
+}
